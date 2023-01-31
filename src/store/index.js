895e25b8b49362e44bf46243f6837832fac3579f
luoyaozi <1085938273@qq.com>
2023-01-31 23:42:13 +0800

    第一次提交

---
+++ src/store/index.js
@@ -0,0 +1,26 @@
+import Vue from "vue";
+import Vuex from "vuex";
+import category from "./module/category";
+import userList from "./module/userList";
+
+Vue.use(Vuex);
+
+
+export default {
+    state:{
+
+    },
+    mutations:{
+
+    },
+    getters:{
+
+    },
+    actions:{
+
+    },
+    module:{
+        category,
+        userList
+    }
+}
